88869dea650ccb1ecaba2519959a212e4187eadd
Md Mohtesham Azam <mdmohteshamazam@outlook.com>
2021-03-31 17:37:40 +0530

    hosting

---
+++ script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide up btn
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // typing animated
+    var typed = new Typed(".typing", {
+        strings: ["\"Engineer\"", "\"Coder\"", "\"Front-end Developer\""],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["\"Engineer\"", "\"Coder\"", "\"Front-end Developer\""],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    // toggle menu btn
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i').toggleClass("active")
+    });
+
+    //drop down qualification card
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("mouseover", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            content.style.maxHeight = content.scrollHeight + "px";
+        });
+        coll[i].addEventListener("mouseout", function() {
+            this.classList.toggle("active");            
+            var content = this.nextElementSibling;
+            content.style.maxHeight = null
+        });
+    }
+});
