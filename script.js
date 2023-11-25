355791df3ec986e6fed0588e475772878611fae6
Mohtesham-azam <78083475+Mohtesham-azam@users.noreply.github.com>
2023-11-25 18:47:14 +0530

    added immage added experience & certificate section added slideshow in home section update skill section added contact no in contact section modified project section

--- script.js
+++ script.js
@@ -19,15 +19,15 @@ $(document).ready(function(){
 
     // typing animated
     var typed = new Typed(".typing", {
-        strings: ["\"Engineer\"", "\"Coder\"", "\"Front-end Developer\""],
-        typeSpeed: 100,
-        backSpeed: 60,
+        strings: ["\"Software Engineer\"", "\"Programmer\"", "\"Full Stack Developer\""],
+        typeSpeed: 50,
+        backSpeed: 10,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["\"Engineer\"", "\"Coder\"", "\"Front-end Developer\""],
-        typeSpeed: 100,
-        backSpeed: 60,
+        strings: ["\"Software Engineer\"", "\"Programmer\"", "\"Full Stack Developer\""],
+        typeSpeed: 50,
+        backSpeed: 10,
         loop: true
     });
     
@@ -52,4 +52,20 @@ $(document).ready(function(){
             content.style.maxHeight = null
         });
     }
+
+    //slideshow
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
 });
